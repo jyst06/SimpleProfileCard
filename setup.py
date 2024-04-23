ac6c2f8f61eb9b6ddb96cbdee08b5bf856acc9ab
JamesYu <107468548+jyst06@users.noreply.github.com>
2024-04-24 00:04:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
     name='profilecard',
     version='1.0.3',
