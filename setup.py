2a0f32bc09339f8dc02cea32bbd94cf53e3314b1
jyst06 <107468548+jyst06@users.noreply.github.com>
2024-04-23 23:38:01 +0800

    add something to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='profilecard',
     version='1.0.3',
     packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     include_package_data=True,
     package_data={'': ['*.ini']},
     install_requires=[
