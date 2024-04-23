5f11ecfc8a3954929d8235e090b65b47e6f37a33
jyst06 <107468548+jyst06@users.noreply.github.com>
2024-04-23 23:05:18 +0800

    1.0.3
    setup file add ini included

--- profilecard/__init__.py
+++ profilecard/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from .simple import Pack, show
 from .simple import set_val, get_val
--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='profilecard',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['*.ini']},
     install_requires=[
         'beautifulsoup4',
         'requests',
